ce7327d4222d65cb2d418cba151785b2b6b1cdde
Luigi Selmi <Luigi.Selmi@iais.fraunhofer.de>
2016-11-29 17:19:56 +0100

    load the database and create two tables for computation

--- init-thessaloniki-db.sh
+++ init-thessaloniki-db.sh
@@ -7,3 +7,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE EXTENSION hstore;
     CREATE EXTENSION  postgis;
 EOSQL
+
+psql -U postgres thessaloniki < bde_pilot_thessaloniki_dump 
+psql -U postgres -d thessaloniki -f CREATE_ways_spatial.sql
+psql -U postgres -d thessaloniki -f CREATE_fn_mapmatch.sql
