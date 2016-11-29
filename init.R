5357d006426c122ed1178f0195f91e1737d3f9f6
Luigi Selmi <Luigi.Selmi@iais.fraunhofer.de>
2016-11-29 17:18:02 +0100

    test the database and the map matching function

---
+++ init.R
@@ -0,0 +1,12 @@
+#!/usr/bin/env Rscript
+# Test the connection to the database
+require("RPostgreSQL")
+driver <- dbDriver("PostgreSQL")
+connection <- dbConnect(driver,dbName="thessaloniki",host="localhost",port=5432,user="postgres",password="password")
+dbExistsTable(connection,"ways_spatial")
+
+# Test the map matching function
+source("mapmatchfunctions_v2.R")
+loadPackages();
+gdata <- read.table("taxi-gps-sample.csv",header=FALSE)
+test_matches <- match(gdata,'localhost',5432,"thessaloniki","postgres","password")
